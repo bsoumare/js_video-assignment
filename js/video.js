fb1cb6ea23c515d17a43b34d7bff225f45143236
bsoumare <144201737+bsoumare@users.noreply.github.com>
2023-11-06 15:29:50 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -3,13 +3,25 @@ var video;
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.pause();
+	video.loop = false
 });
+
 
 document.querySelector("#play").addEventListener("click", function() {
- 	console.log("Play Video");
+	console.log("Play Video");
 });
 
-function load() {
-	video.pause();
-}
+
+//let video = document.getElementById("myVideo"); 
+
+//function play() { 
+//  vid.play(); 
+//} 
+
+//function pause() { 
+//  vid.pause(); 
+//} 
 

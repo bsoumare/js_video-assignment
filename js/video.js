303f781e8d8c6ec9ffbc4d8097ba47f76e717e85
bsoumare <144201737+bsoumare@users.noreply.github.com>
2023-11-08 01:45:31 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -14,6 +14,58 @@ document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 });
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+});
+
+let video = document.getElementById("slower");
+video.playbackRate = 0.1;
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Speed of the Video is   ");
+});
+
+let video = document.getElementById("faster");
+video.playbackRate = 1.1;
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed of the Video is   ");
+});
+
+function skip(value) {
+	var video = 10;
+	var video = document.getElementById("skip");
+	video.currentTime += value;
+	if (video.currentTime){
+        console.log("Time exceeds so video restarts")
+
+    }
+    else{
+		console.log("Add ten seconds")
+    }
+}  
+
+function restart() {
+	var video = document.getElementById("video");
+	video.currentTime = 0;
+}
+
+let vid = document.getElementById("mute");
+video.muted = true;
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Video is now muted");
+});
+
+$("vintage").click(function(){
+	$("p").addClass("vintage");
+  });
+
+  
+$("orig").click(function(){
+	$("p").removeClass("vintage");
+  });
+
 
 //let video = document.getElementById("myVideo"); 
 
